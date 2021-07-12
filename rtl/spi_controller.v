1b93c4169eff75e78491aebbd9112ee34a08a878
Joseph Bellahcen <joeclb@icloud.com>
2021-07-12 10:15:43 -0500

    Vivado-synthesizable

--- rtl/spi_controller.v
+++ rtl/spi_controller.v
@@ -42,13 +42,13 @@ module spi_controller(
 
     // Operational states
     localparam [2:0]
-        RESET           = 'd1,
-        READY           = 'd2,
-        CONFIG          = 'd3,
-        WAIT_FOR_CLOCK  = 'd4,
-        TXRX            = 'd5,
-        TXRX_FINAL      = 'd6,
-        RX_VALID        = 'd7;
+        RESET           = 'd0,
+        READY           = 'd1,
+        CONFIG          = 'd2,
+        WAIT_FOR_CLOCK  = 'd3,
+        TXRX            = 'd4,
+        TXRX_FINAL      = 'd5,
+        RX_VALID        = 'd6;
 
     reg [2:0] r_state;
     reg [2:0] r_next_state;
@@ -60,10 +60,10 @@ module spi_controller(
     reg       r_copi;
 
     // SPI configuration
-    reg [1:0] r_spi_mode;
-    reg [7:0] r_clk_ratio;
-    reg       r_cpol;
-    reg       r_cphase;
+    wire [1:0] w_spi_mode;
+    wire [7:0] w_clk_ratio;
+    wire       w_cpol;
+    wire       w_cphase;
 
     // SPI clock generator
     reg [8:0]  r_clk_config;
@@ -87,10 +87,16 @@ module spi_controller(
         .o_falling_edge(w_falling_edge),
         .o_slow_count(w_clk_count)
     );
+    
+    // Assignments
+    assign w_spi_mode  = r_config[1:0];
+    assign w_clk_ratio = r_config[9:2];
+    assign w_cpol = (w_spi_mode == 2) || (w_spi_mode == 3);
+    assign w_cphase = (w_spi_mode == 1) || (w_spi_mode == 3);
 
     always @(posedge i_clk) begin
         if (~i_rst_n) begin
-            r_config <= 0;
+            r_config <= 10'b00000010_00;
             r_tx <= 0;
             r_rx <= 0;
             r_copi <= 0;
@@ -111,12 +117,12 @@ module spi_controller(
         end
 
         else if (r_state == TXRX) begin
-            if ((r_cphase && w_rising_edge) || (~r_cphase && w_falling_edge)) begin
+            if ((w_cphase && w_rising_edge) || (~w_cphase && w_falling_edge)) begin
                 r_copi <= r_tx[7];
                 r_tx <= r_tx << 1;
             end
 
-            else if ((~r_cphase && w_rising_edge) || (r_cphase && w_falling_edge))
+            else if ((~w_cphase && w_rising_edge) || (w_cphase && w_falling_edge))
                 r_rx <= {r_rx[6:0], i_cipo};
 
             r_state <= r_next_state;
@@ -135,30 +141,19 @@ module spi_controller(
 
     always @(*) begin
         // Default register values
+        r_clk_config = 0;
+        r_clk_start = 1;
         r_next_state = r_state;
 
         // Update internal registers
         case (r_state)
             RESET: begin
-                r_spi_mode  = 2'h0;
-                r_clk_ratio = 8'h2;
-
-                r_cpol   = 0;
-                r_cphase = 0;
-
                 r_clk_start = 1;
-
                 r_next_state = WAIT_FOR_CLOCK;
             end
 
             CONFIG: begin
-                r_spi_mode  = r_config[1:0];
-                r_clk_ratio = r_config[9:2];
-
-                r_cpol   = (r_spi_mode == 2) || (r_spi_mode == 3);
-                r_cphase = (r_spi_mode == 1) || (r_spi_mode == 3);
-
-                r_clk_config = {r_clk_ratio, 1'd1};
+                r_clk_config = {w_clk_ratio, 1'd1};
 
                 // Wait for clock to enter CONFIG state
                 if (w_clk_idle)
@@ -171,11 +166,8 @@ module spi_controller(
 
             WAIT_FOR_CLOCK: begin
                 // Disable clock when it enters IDLE state
-                if (w_clk_idle) begin
-                    r_clk_start = 1;
-                    r_clk_config = 0;
+                if (w_clk_idle)
                     r_next_state = READY;
-                end
             end
 
             TXRX: begin
@@ -194,7 +186,7 @@ module spi_controller(
         o_ready     = (r_state == READY) || (r_state == RX_VALID);
         o_rx_valid  = (r_state == RX_VALID);
         o_rx        = r_rx;
-        o_sclk      = r_cpol ? w_clk_n : w_clk;
+        o_sclk      = w_cpol ? w_clk_n : w_clk;
         o_copi      = r_copi;
     end
 endmodule
