d110f896eae863a46174b082c1459a20a93e7407
Joseph Bellahcen <joe@josephbellahcen.com>
2021-08-02 12:18:22 -0500

    Synthesizable imrpoved SPI top

--- rtl/spi_controller_top.v
+++ rtl/spi_controller_top.v
@@ -9,56 +9,160 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
-module spi_controller_top
-    (
-        input           i_clk,
-        input           i_rst_n,
+module spi_controller_top(
+        // FPGA interface
+        input i_clk,
+        input i_rst_n,
 
-        input [10:0]    i_config,
-        input [7:0]     i_tx,
-        input           i_tx_valid,
-    
-        output [7:0]    o_rx,
-        output          o_rx_valid,
-        
-        input           i_cipo,
-        output          o_copi,
-        output          o_sclk,
-        
-        output          o_ready
+        // Control interface
+        input           i_request_tx,
+        input           i_ws_n,
+        input           i_rs_n,
+        input [2:0]     i_addr,
+
+        // Data busses
+        input   [7:0]     i_data,
+        output  [7:0]     o_data,
+
+        // SPI
+        input   i_cipo,
+        output  o_copi,
+        output  o_sclk,
+
+        // Status registers
+        output o_ready,
+        output o_rx_valid
     );
+
+    // Debug registers (read-only)
+    wire [7:0] w_whoami;
+
+    // Control registers (read-write)
+    reg [1:0] r_spi_mode,       r_next_spi_mode;
+    reg [7:0] r_clk_div,        r_next_clk_div;
+    reg       r_config_flag,    r_next_config_flag;
+
+    // Data bus output
+    wire [7:0] w_spi_rx;
+    reg  [7:0] r_data, r_next_data;
     
     // Synchronized inputs
-    reg [1:0] r_config_sync;
-    reg [1:0] r_tx_valid_sync;
-    reg [1:0] r_cipo_sync;
+    wire w_request_tx_sync;
+    wire w_ws_n_sync, w_rs_n_sync;
+    wire w_cipo_sync;
+    sync S0(.i_clk(i_clk), .i_rst_n(i_rst_n), .i_sig(i_request_tx), .o_stable(w_request_tx_sync));
+    sync S1(.i_clk(i_clk), .i_rst_n(i_rst_n), .i_sig(i_ws_n),       .o_stable(w_ws_n_sync));
+    sync S2(.i_clk(i_clk), .i_rst_n(i_rst_n), .i_sig(i_rs_n),       .o_stable(w_rs_n_sync));
+    sync S3(.i_clk(i_clk), .i_rst_n(i_rst_n), .i_sig(i_cipo),       .o_stable(w_cipo_sync));
     
     spi_controller SC(
     	.i_clk(i_clk),
     	.i_rst_n(i_rst_n),
-    	.i_config({i_config[10:1], r_config_sync[1]}),   
-    	.i_tx(i_tx),
-    	.i_tx_valid(r_tx_valid_sync[1]),
-    	.o_rx({o_rx}),
+    	.i_config({r_clk_div, r_spi_mode, r_config_flag}),   
+    	.i_tx(i_data),
+    	.i_tx_valid(i_request_tx),
+    	.o_rx(w_spi_rx),
     	.o_rx_valid(o_rx_valid),
-    	.i_cipo(r_cipo_sync[1]),
+    	.i_cipo(w_cipo_sync),
     	.o_copi(o_copi),
     	.o_sclk(o_sclk),
     	.o_ready(o_ready)
     );
-    
-    // Input synchronization
+
+    // FSM
+    localparam [5:0]
+        READY       = 6'b000001,
+        REQUEST_TX  = 6'b000010,
+        SPI_RUN     = 6'b000100,
+        READ_REG    = 6'b001000,
+        WRITE_REG   = 6'b010000,
+        CONFIG      = 6'b100000;
+
+    reg [5:0] r_state, r_next_state;
+
     always @(posedge i_clk) begin
         if (~i_rst_n) begin
-            r_config_sync   <= 2'b00;
-            r_tx_valid_sync <= 2'b00;
-            r_cipo_sync     <= 2'b00;
+            r_state         <= READY;
+            r_spi_mode      <= 'h0;
+            r_clk_div       <= 'h2;
+            r_config_flag   <= 'h0;
+            r_data          <= 'h0;
         end
-        
+
         else begin
-            r_config_sync   <= {r_config_sync[0], i_config[0]};
-            r_tx_valid_sync <= {r_tx_valid_sync[0], i_tx_valid};
-            r_cipo_sync     <= {r_cipo_sync[0], i_cipo};
+            r_state         <= r_next_state;
+            r_spi_mode      <= r_next_spi_mode;
+            r_clk_div       <= r_next_clk_div;
+            r_config_flag   <= r_next_config_flag;
+            r_data          <= r_next_data;
         end
     end
+
+    always @(*) begin
+        r_next_state        = r_state;
+        r_next_spi_mode     = r_spi_mode;
+        r_next_clk_div      = r_clk_div;
+        r_next_config_flag  = r_config_flag;
+        r_next_data         = r_data;
+
+        case (r_state)
+            READY: begin
+                if (w_request_tx_sync)
+                    r_next_state = REQUEST_TX;
+
+                else if (~w_rs_n_sync)
+                    r_next_state = READ_REG;
+
+                else if (~w_ws_n_sync)
+                    r_next_state = WRITE_REG;
+
+                else if (o_rx_valid)
+                    r_next_data = w_spi_rx;
+            end
+
+            REQUEST_TX: begin
+                r_next_state = o_ready ? SPI_RUN : READY;
+            end
+
+            SPI_RUN: begin
+                if (o_rx_valid) begin
+                    r_next_data = w_spi_rx;
+                    r_next_state = READY;
+                end
+            end
+
+            READ_REG: begin
+                case (i_addr)
+                    0:  r_next_data = 'h0;
+                    1:  r_next_data = 'hff;
+                    2:  r_next_data = w_whoami;
+                    3:  r_next_data = r_spi_mode;
+                    4:  r_next_data = r_clk_div;
+                endcase
+                
+                r_next_state = READY;
+            end
+
+            WRITE_REG: begin
+                case (i_addr)
+                    3:  r_next_spi_mode = i_data[1:0];
+                    4:  r_next_clk_div  = i_data;
+                endcase
+
+                r_next_config_flag = 'h1;
+                r_next_state = CONFIG;
+            end
+
+            CONFIG: begin
+                r_next_config_flag = 'h0;
+                r_next_state = READY;
+            end
+        endcase
+    end
+
+    // Debug registers
+    assign w_whoami = 8'hA7;
+
+    // Outputs
+    assign o_data = r_data;
 endmodule
--- sim/spi_controller_tb.sv
+++ sim/spi_controller_tb.sv
@@ -3,15 +3,17 @@
 module spi_controller_tb;
     logic           i_clk;
     logic           i_rst_n;
-    logic [10:0]    i_config;
-    logic [7:0]     i_tx;
-    logic           i_tx_valid;
+    logic           i_request_tx;
+    logic           i_ws_n;
+    logic           i_rs_n;
+    logic [2:0]     i_addr;
+    logic [7:0]     i_data;
+    logic [7:0]     o_data;
     logic           i_cipo;
-    logic           o_ready;
-    logic [7:0]     o_rx;
-    logic           o_rx_valid;
     logic           o_copi;
     logic           o_sclk;
+    logic           o_ready;
+    logic           o_rx_valid;
 
     // 100 MHz clock
     always #5 i_clk = ~i_clk;
@@ -20,19 +22,7 @@ module spi_controller_tb;
 
     integer n_iter = 255;
 
-    spi_controller DUT(
-        .i_clk,
-        .i_rst_n,
-        .i_config,
-        .i_tx,
-        .i_tx_valid,
-        .i_cipo,
-        .o_ready,
-        .o_rx,
-        .o_rx_valid,
-        .o_copi,
-        .o_sclk
-    );
+    spi_controller_top DUT(.*);
 
     initial begin
         $dumpfile("spi_controller.vcd");
@@ -41,32 +31,34 @@ module spi_controller_tb;
 
     initial begin
         $display("Simulation start");
-        i_clk = 0;
-        i_rst_n = 1;
-        i_config = 0;
-        i_tx = 0;
-        i_tx_valid = 0;
-        i_cipo = 0;
+        i_clk           = 'h0;
+        i_rst_n         = 'h1;
+        i_request_tx    = 'h0;
+        i_ws_n          = 'h1;
+        i_rs_n          = 'h1;
+        i_addr          = 'h0;
+        i_data          = 'h0;
+        i_cipo          = 'h0;
 
         reset;
 
         $display("MODE 0 TESTS");
-        configure(0, 2);
+        configure(0, 4);
         repeat (n_iter)
             send_byte($random, $random);
 
         $display("MODE 1 TESTS");
-        configure(1, 4);
+        configure(1, 8);
         repeat (n_iter)
             send_byte($random, $random);
 
         $display("MODE 2 TESTS");
-        configure(2, 6);
+        configure(2, 12);
         repeat (n_iter)
             send_byte($random, $random);
 
         $display("MODE 3 TESTS");
-        configure(3, 8);
+        configure(3, 16);
         repeat (n_iter)
             send_byte($random, $random);
 
@@ -100,12 +92,26 @@ module spi_controller_tb;
         $display("Configuring...");
         $display("MODE: %d, CLOCK SPEED: %f MHz", spi_mode, (1.0/clk_ratio) * 100);
 
-        @(posedge i_clk)
-            #t_in i_config = {clk_ratio, spi_mode, 1'b1};
+        repeat (2) @(posedge i_clk) begin
+            i_addr = 'h3;
+            i_data = spi_mode;
+            #t_in i_ws_n = 'h0;
+        end
 
         @(posedge i_clk)
-            #t_in i_config = 'h0;
+            #t_in i_ws_n = 'h1;
 
+        repeat (2) @(posedge i_clk) begin
+            i_addr = 'h4;
+            i_data = clk_ratio;
+            #t_in i_ws_n = 'h0;
+        end
+
+        @(posedge i_clk)
+            #t_in i_ws_n = 'h1;
+
+        i_addr = 'h0;
+        i_data = 'h0;
     endtask
 
     task send_byte;
@@ -115,29 +121,32 @@ module spi_controller_tb;
         $display("Sending x%x\tReceiving x%x", data, rx);
 
         @(posedge i_clk) begin
-            i_tx = data;
-            i_cipo = 0;
-            #t_in i_tx_valid = 1;
+            i_data = data;
+            i_cipo = 'h0;
+            #t_in i_request_tx = 'h1;
         end
         
         // Wait for command to register
         if (o_ready)
             @(negedge o_ready)
                 @(posedge i_clk)
-                    i_tx_valid = 0;
+                    i_request_tx = 'h0;
                     
         // Send dummy peripheral data
         for (int i = 7; i >= 0; i--) begin
             @(posedge o_sclk) begin
-                i_cipo = rx[i];
+                #t_in i_cipo = rx[i];
             end
         end
 
         // Wait for command to finish
         @(posedge o_rx_valid) begin
+            @(posedge i_clk)
+                #t_in i_cipo = 'h0;
+                
             @(posedge i_clk) begin
-                $display("Received x%x", o_rx);
-                #t_out assert(o_rx === rx) else $fatal(1);
+                $display("Received x%x", o_data);
+                #t_out assert(o_data === rx) else $fatal(1);
             end
         end
     endtask
